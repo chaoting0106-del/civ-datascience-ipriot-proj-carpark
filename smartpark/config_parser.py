ae8572a9439e891e3e484c74f5699edd52c4d9ea
Chao-ting Yen <j277184@tafe.wa.edu.au>
2026-06-17 12:30:09 +0800

    fix config

--- smartpark/config_parser.py
+++ smartpark/config_parser.py
@@ -1,43 +1,6 @@
-"""A class or function to parse the config file and return the values as a dictionary.
-
-The config file itself can be any of the following formats (recommend one of pandas, json, or ryo):
-
-- You can use pandas to read a data file if you like. Something simple like a CSV would be best.
-
-- ryo: means 'roll your own' and is a simple text file with key-value pairs separated by an equals sign. For example:
-```
-location = "Moondalup City Square Parking"
-number_of_spaces = 192
-```
-**you** read the file and parse it into a dictionary.
-- json: a json file with key-value pairs. For example:
-```json
-{location: "Moondalup City Square Parking", number_of_spaces: 192}
-```
-json is built in to python, so you can use the json module to parse it into a dictionary.
-- toml: a toml file with key-value pairs. For example:
-```toml
-[location]
-name = "Moondalup City Square Parking"
-spaces = 192
-```
-toml is part of the standard library in python 3.11, otherwise you need to install tomli to parse it into a dictionary.
-```bash
-python -m pip install tomli
-```
-see [realpython.com](https://realpython.com/python-toml/) for more info.
-
-Finally, you can use `yaml` if you prefer.
-
-
-
-"""
-
-
-
 def parse_config(config_file: str) -> dict:
-    """Parse the config file and return the values as a dictionary"""
-    import json
+    """Set up config into json file"""
+    import json    
     with open(config_file) as input_file:
         config = json.load(input_file)
     return config["CarParks"][0]
